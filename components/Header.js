2f6ad5c2358976e99fdbaa22c8e0edd285ea0c7a
Dayan Arango <dayaara6@gmail.com>
2022-04-28 11:42:12 -0400

    añadir boton carrito

--- components/Header.js
+++ components/Header.js
@@ -22,6 +22,11 @@ const Header = ({guitarra}) => {
             <Link href="/nosotros">Nosotros</Link>
             <Link href="/blog">Blog</Link>
             <Link href="/tienda">Tienda</Link>
+            <Link href="/carrito">
+              <a>
+                <Image layout="fixed" width={30} height={25} src='/img/carrito.png' alt="carrito" />
+              </a>
+            </Link>
           </nav>
         </div>
         {guitarra && (
